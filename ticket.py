3dff9fb94d91df8f220a550176cf5a5e51c99067
irisyang0808 <iris88.yang@gmail.com>
2024-10-20 20:30:56 +0800

    Verification code remaining

--- ticket.py
+++ ticket.py
@@ -1,31 +1,67 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
 
 # 開啟網頁
 #driver.get("https://www.google.com")
-driver.get("https://tixcraft.com/")
+#driver.get("https://tixcraft.com/activity/game/24_jaychou")
+driver.get("https://tixcraft.com/activity/game/25_casty")
+driver.maximize_window()
+
+if(driver.find_element(By.CLASS_NAME, "banner-content")):
+    reject_button = driver.find_element(By.ID, "onetrust-reject-all-handler").click()
+
+time.sleep(1)
+
+# ctrl+shift+i查看網頁原始代碼
+# 會員登入
+login_button = driver.find_element(By.CLASS_NAME, "justify-content-center").click()
+fb_button = driver.find_element(By.ID, "facebook").click()
+
+# login facebook
+account_fill = driver.find_element(By.ID, "email")
+account_fill.send_keys("your email")
+password_fill = driver.find_element(By.ID, "pass")
+password_fill.send_keys("tour password")
+login = driver.find_element(By.NAME, "login").click()
+continue_button = driver.find_elements(By.CSS_SELECTOR, '[aria-label="以XX的身分繼續"]')
+continue_button[0].click()
+
+#不斷刷新直到下拉式選單出現
+
+#滾動視窗到底部
+#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 time.sleep(5)
 
-#ctrl+shift+i查看網頁原始代碼
-#會員登入
-login_button = driver.find_element(By.CLASS_NAME, "justify-content-center")
-login_button.click()
-fb_button = driver.find_element(By.ID, "facebook")
-fb_button.click()
+# choose day
+select_day = driver.find_elements(By.TAG_NAME, value="Option")
+i=0
+while i < len(select_day):
+    if(select_day[i].text == "2025/03/22 (六)"):
+        select_day[i].click
+    i=i+1
+
+buy_button = driver.find_element(By.CSS_SELECTOR, '[class="btn btn-primary text-bold m-0"]').click()
+
+#choose zone & ticket num
+select_zone = driver.find_element(By.CLASS_NAME, "select_form_a").click()
+select_num = Select(driver.find_element(By.ID, "TicketForm_ticketPrice_02"))
+select_num.select_by_index(2)
+
+# verifyCode
+verifyCode_fill = driver.find_element(By.ID, "TicketForm_verifyCode")
+verifyCode_fill.send_keys("ai圖片辨識結果回傳")
+checkbox = driver.find_element(By.ID, "TicketForm_agree").click()
+confirm_button = driver.find_element(By.CSS_SELECTOR, '[class="btn btn-primary btn-green"]').click()
+confirm_order_button = driver.find_element(By.ID, "submitButton").click()
 
 
-time.sleep(5)
+time.sleep(100)
 
-
-
-# search_box = driver.find_element("name", "q")  # "q" 是 Google 搜尋框的 name 屬性
-# search_box.send_keys("Selenium Python")  # 在搜尋框中輸入 "Selenium Python"
-# search_box.send_keys(Keys.RETURN)  # 模擬按下 Enter 鍵
-
-# 記得在完成操作後關閉瀏覽器
+# 關閉瀏覽器
 #driver.quit()
